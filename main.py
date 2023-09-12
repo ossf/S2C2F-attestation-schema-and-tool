68d7c36c31e195b1b181f9babe7dbe52357eeb03
anandchugh <3197424+anandchugh@users.noreply.github.com>
2023-09-13 01:09:45 +0530

    adding github workflow
    Signed-off-by: anandchugh <anandch@microsoft.com>

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+
+url = "https://www.bing.com/search?q=oscal"
+
+
+def perform_search(url):
+    try:
+        print("Requesting", url)
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response
+            print("API Response:", data)
+            return response.status_code
+        else:
+            print(
+                "Error: Request to the API failed with status code",
+                response.status_code,
+            )
+    except requests.exceptions.RequestException as e:
+        print("Request Exception:", e)
+
+
+if __name__ == "__main__":
+    perform_search(url)
